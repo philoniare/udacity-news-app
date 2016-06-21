cd9265c71357b5523e6e2edf8acedb7ea7b9b7eb
philoniare <philoniare@gmail.com>
2016-06-21 14:04:38 -0400

    Added empty_view

--- app/src/main/java/com/example/philoniare/newsapp/view/MainActivity.java
+++ app/src/main/java/com/example/philoniare/newsapp/view/MainActivity.java
@@ -5,14 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.philoniare.newsapp.GuardianAPI.GuardianClient;
 import com.example.philoniare.newsapp.GuardianAPI.GuardianMain;
 import com.example.philoniare.newsapp.GuardianAPI.GuardianResponse;
 import com.example.philoniare.newsapp.GuardianAPI.GuardianResult;
 import com.example.philoniare.newsapp.GuardianAPI.GuardianServiceGenerator;
-import com.example.philoniare.newsapp.adapter.NewsAdapter;
 import com.example.philoniare.newsapp.R;
+import com.example.philoniare.newsapp.adapter.NewsAdapter;
 import com.example.philoniare.newsapp.model.NewsArticle;
 
 import java.util.ArrayList;
@@ -28,6 +30,7 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
     public static List<NewsArticle> newsArticles;
     @Bind(R.id.news_list) RecyclerView newsList;
+    @Bind(R.id.empty_view) TextView emptyView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +43,17 @@ public class MainActivity extends AppCompatActivity {
         NewsAdapter newsAdapter = new NewsAdapter(MainActivity.this, newsArticles);
         newsList.setAdapter(newsAdapter);
         fetchNewsArticles("artificialintelligenceai");
+    }
+
+    private void reDrawRecyclerView() {
+        if(newsArticles.isEmpty()) {
+            newsList.setVisibility(View.GONE);
+            emptyView.setVisibility(View.VISIBLE);
+        } else {
+            newsList.setVisibility(View.VISIBLE);
+            emptyView.setVisibility(View.GONE);
+        }
+        newsList.getAdapter().notifyDataSetChanged();
     }
 
     private void fetchNewsArticles(String topic) {
@@ -63,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
                                 apiResult.getSectionName(), thumbnail,
                                 apiResult.getWebUrl()));
                     }
-                    newsList.getAdapter().notifyDataSetChanged();
+                    reDrawRecyclerView();
                 } else {
                     int statusCode = mainResponse.code();
                     ResponseBody errorBody = mainResponse.errorBody();
